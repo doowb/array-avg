083d77edc47afe5f376cd9eba6202728bee8a621
Brian Woodward <brian.woodward@gmail.com>
2015-09-23 09:31:49 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * array-avg <https://github.com/doowb/array-avg>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
